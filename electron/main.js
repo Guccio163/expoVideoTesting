8b7b6a390c4a42af73c7f9797c8bd0e1fc7e2be6
Wiktor Gut <wiktor.gut@swmansion.com>
2025-08-14 13:36:48 +0200

    testing app

---
+++ electron/main.js
@@ -0,0 +1,28 @@
+// electron/main.js
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  win.loadURL("http://localhost:8081"); // domyślny port Expo Web
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
